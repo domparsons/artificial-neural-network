0a038127a052107ba789de495e710548f09d4a22
Dom Parsons <puffins_fridge0d@icloud.com>
2024-12-09 22:37:20 +0000

    feature: docstrings and type hints

--- models/flood_predictor.py
+++ models/flood_predictor.py
@@ -1,8 +1,18 @@
 import numpy as np
+import pandas as pd
 from scipy.stats import pearsonr
 from sklearn.metrics import mean_absolute_error
 
 class FloodPredictor:
+    """
+    FloodPredictor class for predicting flood index values using a trained neural network.
+
+    Attributes:
+        hidden_layer_weights (np.ndarray): Weights of the hidden layer.
+        hidden_layer_biases (np.ndarray): Biases of the hidden layer.
+        output_layer_weights (np.ndarray): Weights of the output layer.
+        output_layer_bias (np.ndarray): Bias of the output layer.
+    """
 
     def __init__(self, hidden_layer_weights, hidden_layer_biases, output_layer_weights, output_layer_bias):
         self.hidden_layer_weights = hidden_layer_weights
@@ -15,11 +25,33 @@ class FloodPredictor:
         return 1 / (1 + np.exp(-x))
 
     @staticmethod
-    def unstandardise_data(s, min_val, max_val, scaling_factor=0.8):
+    def unstandardise_data(s: float, min_val: float, max_val: float, scaling_factor: float = 0.8) -> float:
+        """"
+        Convert standardised data back to its original scale.
+
+        Parameters:
+        s (float): The standardised value.
+        min_val (float): The minimum value of the original data.
+        max_val (float): The maximum value of the original data.
+        scaling_factor (float): The scaling factor used during standardisation. Default is 0.8.
+
+        Returns:
+        float: The unstandardised value.
+        """
         r = ((s - 0.1) / scaling_factor) * (max_val - min_val) + min_val
         return r
 
-    def predict(self, standardised_testing_data, train_and_validate):
+    def predict(self, standardised_testing_data: pd.DataFrame, train_and_validate: pd.DataFrame) -> list:
+        """
+        Predict the index flood values for the given standardised testing data.
+
+        Parameters:
+        standardised_testing_data (pandas.DataFrame): The standardised testing data.
+        train_and_validate (pandas.DataFrame): The training and validation data used to determine the min and max index flood values.
+
+        Returns:
+        list: The predicted index flood values.
+        """
         min_index_flood = train_and_validate['Index flood'].min()
         max_index_flood = train_and_validate['Index flood'].max()
 
@@ -33,7 +65,18 @@ class FloodPredictor:
         return predictions
 
     @staticmethod
-    def evaluate(y_test, y_predict, threshold=60):
+    def evaluate(y_test: np.ndarray, y_predict: np.ndarray, threshold: int = 60) -> tuple:
+        """
+        Evaluate the performance of the flood prediction model.
+
+        Parameters:
+        y_test (array-like): The true index flood values.
+        y_predict (array-like): The predicted index flood values.
+        threshold (int, optional): The threshold value for precision calculation. Default is 60.
+
+        Returns:
+        tuple: A tuple containing the correlation coefficient, precision, and mean absolute error (MAE).
+        """
         correlation_coefficient, _ = pearsonr(y_test, y_predict)
         within_threshold = np.sum(np.abs(y_test - y_predict) < threshold)
         precision = within_threshold / len(y_test)
--- models/neural_network.py
+++ models/neural_network.py
@@ -1,8 +1,54 @@
+from typing import Any
+
 import numpy as np
 import random
 
-class NeuralNetwork:
+import pandas as pd
+from numpy import floating
 
+
+class NeuralNetwork:
+    """
+    A class used to represent a Neural Network.
+
+    Attributes
+    ----------
+    input_size : int
+        The number of input features.
+    hidden_layer_size : int
+        The number of neurons in the hidden layer.
+    output_size : int
+        The number of output neurons.
+    hidden_layer_weights : np.ndarray
+        Weights of the hidden layer.
+    hidden_layer_biases : np.ndarray
+        Biases of the hidden layer.
+    output_layer_weights : np.ndarray
+        Weights of the output layer.
+    output_layer_bias : float
+        Bias of the output layer.
+    previous_output_weight_change : np.ndarray
+        Previous weight change for the output layer (for momentum).
+    previous_hidden_weight_change : np.ndarray
+        Previous weight change for the hidden layer (for momentum).
+
+    Methods
+    -------
+    train(training_data, validation_data, epochs, initial_learning_rate, final_learning_rate, momentum_rate, epoch_split)
+        Trains the neural network using the provided training and validation data.
+    forward_pass(inputs)
+        Performs a forward pass through the neural network.
+    backward_pass(inputs, output_activations, hidden_layer_activations)
+        Performs a backward pass through the neural network to calculate deltas.
+    update_weights(inputs, hidden_activations, deltas, learning_rate, momentum_rate)
+        Updates the weights of the neural network using the calculated deltas, learning rate, and momentum rate.
+    calculate_mean_validation_error(validation_data)
+        Calculates the mean validation error for the neural network.
+    sigmoid(x)
+        Applies the sigmoid activation function.
+    simulated_annealing(start_lr, end_lr, max_epochs, current_epoch)
+        Calculates the learning rate using simulated annealing.
+    """
     def __init__(self, input_size, hidden_layer_size, output_size):
         self.input_size = input_size
         self.hidden_layer_size = hidden_layer_size
@@ -17,91 +63,118 @@ class NeuralNetwork:
         self.previous_output_weight_change = np.zeros_like(self.output_layer_weights)
         self.previous_hidden_weight_change = np.zeros_like(self.hidden_layer_weights)
 
-    def train(self, training_data, validation_data, epochs, initial_learning_rate, final_learning_rate, momentum_rate,
-              epoch_split):
+    def train(self, training_data: pd.DataFrame, validation_data: pd.DataFrame, epochs: int,
+              initial_learning_rate: float,
+              final_learning_rate: float, momentum_rate: float, epoch_split: int) -> tuple:
+        """
+        Train the neural network using the provided training and validation data.
+
+        Parameters:
+        training_data (pd.DataFrame): The data used for training the neural network.
+        validation_data (pd.DataFrame): The data used for validating the neural network.
+        epochs (int): The number of epochs to train the neural network.
+        initial_learning_rate (float): The initial learning rate for training.
+        final_learning_rate (float): The final learning rate for training.
+        momentum_rate (float): The momentum rate for weight updates.
+        epoch_split (int): The interval at which to calculate and print the mean validation error.
+
+        Returns:
+        tuple: A tuple containing the mean validation errors, hidden layer weights, hidden layer biases, output layer weights, and output layer bias.
+        """
         mean_validation_errors = []
 
         for epoch in range(epochs):
-            # Update the learning rate
             learning_rate = self.simulated_annealing(initial_learning_rate, final_learning_rate, epochs, epoch)
 
-            # Iterate through the training data rows
             for _, row in training_data.iterrows():
-                # Forward pass
                 hidden_layer_activations, output_layer_activations = self.forward_pass(row.values)
-
-                # Backward pass
                 deltas = self.backward_pass(row.values, output_layer_activations, hidden_layer_activations)
 
-                # Update weights and biases
                 self.update_weights(row.values, hidden_layer_activations, deltas, learning_rate, momentum_rate)
 
             if epoch % epoch_split == 0:
-                # Calculate mean validation error
                 mean_validation_error = self.calculate_mean_validation_error(validation_data)
-
-                # Print the mean validation error for the current epoch
                 print(f"{epoch}: Error: {mean_validation_error}")
-
-                # Append the mean validation error to the list of mean validation errors
                 mean_validation_errors.append(mean_validation_error)
 
         return mean_validation_errors, self.hidden_layer_weights, self.hidden_layer_biases, self.output_layer_weights, self.output_layer_bias
 
-    def forward_pass(self, inputs):
-        # Calculate weighted sums at the hidden layer
+    def forward_pass(self, inputs: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Perform a forward pass through the neural network.
+
+        Parameters:
+        inputs (np.ndarray): The input data.
+
+        Returns:
+        tuple: A tuple containing the hidden layer activations and output layer activations.
+        """
         hidden_layer_weighted_sums = np.dot(inputs, self.hidden_layer_weights) + self.hidden_layer_biases
-        # Apply sigmoid activation to get activations at the hidden layer
         hidden_layer_activations = self.sigmoid(hidden_layer_weighted_sums)
 
-        # Calculate weighted sums at the output layer
-        output_layer_weighted_sums = np.dot(hidden_layer_activations,
-                                            self.output_layer_weights) + self.output_layer_bias
-        # Apply sigmoid activation to get activations at the output layer
+        output_layer_weighted_sums = np.dot(hidden_layer_activations, self.output_layer_weights) + self.output_layer_bias
         output_layer_activations = self.sigmoid(output_layer_weighted_sums)
 
         return hidden_layer_activations, output_layer_activations
 
-    def backward_pass(self, inputs, output_activations, hidden_layer_activations):
-        # Initialise an empty list to store the deltas
-        deltas = []
+    def backward_pass(self, inputs: np.ndarray, output_activations: np.ndarray,
+                      hidden_layer_activations: np.ndarray) -> list:
+        """
+        Perform a backward pass through the neural network to calculate deltas.
 
-        # Calculate the delta for the output layer
-        deltas.append((inputs[-1] - output_activations[0]) * (output_activations[0] * (1 - output_activations[0])))
+        Parameters:
+        inputs (np.ndarray): The input data.
+        output_activations (np.ndarray): The activations from the output layer.
+        hidden_layer_activations (np.ndarray): The activations from the hidden layer.
 
-        # Calculate the deltas for the hidden layer nodes
+        Returns:
+        list: A list of deltas for weight updates.
+        """
+        deltas = [(inputs[-1] - output_activations[0]) * (output_activations[0] * (1 - output_activations[0]))]
+
         for x in range(self.hidden_layer_size):
-            # The delta value for each hidden layer node is calculated based on the deltas from the output layer
-            deltas.append((hidden_layer_activations[x] * (1 - hidden_layer_activations[x])) * deltas[x] *
-                          self.hidden_layer_biases[x])
+            deltas.append((hidden_layer_activations[x] * (1 - hidden_layer_activations[x])) * deltas[x] * self.hidden_layer_biases[x])
 
         return deltas
 
-    def update_weights(self, inputs, hidden_activations, deltas, learning_rate, momentum_rate):
-        # Calculate weight changes without momentum
+    def update_weights(self, inputs: np.ndarray, hidden_activations: np.ndarray, deltas: list, learning_rate: float,
+                       momentum_rate: float) -> None:
+        """
+        Update the weights of the neural network using the calculated deltas, learning rate, and momentum rate.
+
+        Parameters:
+        inputs (np.ndarray): The input data.
+        hidden_activations (np.ndarray): The activations from the hidden layer.
+        deltas (list): The calculated deltas for weight updates.
+        learning_rate (float): The learning rate for weight updates.
+        momentum_rate (float): The momentum rate for weight updates.
+        """
         output_weight_change = learning_rate * np.outer(hidden_activations, deltas[0])
         hidden_weight_change = learning_rate * np.outer(inputs, deltas[1:])
 
-        # Apply momentum to weight changes
         output_weight_change += momentum_rate * self.previous_output_weight_change
         hidden_weight_change += momentum_rate * self.previous_hidden_weight_change
 
-        # Update output layer weights and biases
         self.output_layer_weights += output_weight_change
         self.output_layer_bias += learning_rate * deltas[0]
 
-        # Update hidden layer weights and biases
         self.hidden_layer_weights += hidden_weight_change
 
-        # Update previous weight changes
         self.previous_output_weight_change = output_weight_change
         self.previous_hidden_weight_change = hidden_weight_change
 
-    def calculate_mean_validation_error(self, validation_data):
-        # Initialise an empty list to store the errors
+    def calculate_mean_validation_error(self, validation_data: pd.DataFrame) -> floating[Any]:
+        """
+        Calculate the mean validation error for the neural network.
+
+        Parameters:
+        validation_data (pd.DataFrame): The data used for validating the neural network.
+
+        Returns:
+        float: The mean validation error.
+        """
         validation_errors = []
 
-        # Iterate through the rows of the standardised validation data calculating the validation error
         for _, val_row in validation_data.iterrows():
             _, val_output_layer_activations = self.forward_pass(val_row.values)
             error = np.mean((val_row.iloc[-1] - val_output_layer_activations[0]) ** 2) ** 0.5
@@ -109,14 +182,24 @@ class NeuralNetwork:
 
         return np.mean(validation_errors)
 
-    # Activation function
     @staticmethod
     def sigmoid(x):
         return 1 / (1 + np.exp(-x))
 
-    # Update the learning rate but ensure it does not drop below the end learning rate
     @staticmethod
-    def simulated_annealing(start_lr, end_lr, max_epochs, current_epoch):
+    def simulated_annealing(start_lr: float, end_lr: float, max_epochs: int, current_epoch: int) -> float:
+        """
+        Calculate the learning rate using simulated annealing.
+
+        Parameters:
+        start_lr (float): The initial learning rate.
+        end_lr (float): The final learning rate.
+        max_epochs (int): The total number of epochs.
+        current_epoch (int): The current epoch number.
+
+        Returns:
+        float: The calculated learning rate for the current epoch.
+        """
         new_rate = ((end_lr + (start_lr - end_lr)) * (1 - (1 / (1 + np.exp(10 - ((20 * current_epoch) / max_epochs))))))
         if new_rate > end_lr:
             return new_rate
