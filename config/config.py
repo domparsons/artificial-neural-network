5956adefa66da1cc70f7a3d40c3fc94a1017c187
Dom Parsons <puffins_fridge0d@icloud.com>
2025-02-16 09:14:31 +0000

    fix: relative file paths

--- config/config.py
+++ config/config.py
@@ -2,8 +2,8 @@ from dataclasses import dataclass
 
 @dataclass
 class FilePaths:
-    data_file = '../data/FEHDataStudent.xlsx'
-    compare_file: str = '../data/compare-data.xlsx'
+    data_file = 'data/FEHDataStudent.xlsx'
+    compare_file: str = 'data/compare-data.xlsx'
     sheet_name: str = 'Sheet1'
     columns: list = ('AREA', 'BFIHOST', 'FARL', 'FPEXT', 'LDP', 'PROPWET', 'RMED-1D', 'SAAR', 'Index flood')
 
